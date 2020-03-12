a4d6f6e723b3214f49303ee112fd96e01e32c876
Mario Lubenka <mario.lubenka@googlemail.com>
2020-03-12 18:03:10 +0100

    feat: show version number

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import {BingoField} from "./BingoField";
 import {Bingo as BingoContainer} from "./bingo";
 import {StateSaver} from "./StateSaver";
+import versions from "./_versions";
 
 window.onload = () => {
+    document.getElementById('version').innerText = versions.hasOwnProperty('versionLong') ? versions.versionLong : versions.version;
     document.getElementById('reset-cookie').addEventListener('click', () => {
         new StateSaver().clearStates();
         location.reload();
